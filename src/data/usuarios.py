a09714a9d7f00aa67372ca2e2dfba16e45c60644
Rônero <ronerojr@gmail.com>
2025-04-08 12:59:27 -0300

    add => criacao do mockup de usuarios

---
+++ src/data/usuarios.py
@@ -0,0 +1,7 @@
+usuarios_db = [
+    {"login": "adm", "senha": "adm"},
+    {"login": "maria", "senha": "senha123"},
+    {"login": "joao", "senha": "abc123"},
+    {"login": "teste", "senha": "teste123"},
+    {"login": "desenvolvedor", "senha": "dev2025"}
+]
